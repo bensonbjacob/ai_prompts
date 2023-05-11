213ab6c91c3651763343062b0de468e35f8cf484
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-05-10 19:59:25 -0500

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import Feed from '@components/Feed';
+
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover and Share
+        <br className='max-md:hidden' />
+        <span className='orange_gradient text-center'>
+          AI Powered Prompts
+        </span>
+      </h1>
+      <p className='desc text-center'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Dolor dignissimos ipsa, provident cupiditate repudiandae esse
+        alias repellendus!
+      </p>
+
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
