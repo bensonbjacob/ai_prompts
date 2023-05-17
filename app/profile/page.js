90a06d10231f562f29e10f15b43f9b3a675b2772
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-05-17 10:51:01 -0500

    Add ability to view different user profiles

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,7 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import Profile from '@components/Profile';
@@ -10,7 +10,7 @@ const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const [posts, setPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -19,11 +19,11 @@ const MyProfile = () => {
       );
       const data = await response.json();
 
-      setPosts(data);
+      setMyPosts(data);
     };
 
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session?.user.id]);
 
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
@@ -40,8 +40,11 @@ const MyProfile = () => {
           method: 'DELETE',
         });
 
-        const filteredPosts = posts.filter((p) => p._id !== post._id);
-        setPosts(filteredPosts);
+        const filteredPosts = myPosts.filter(
+          (item) => item._id !== post._id
+        );
+
+        setMyPosts(filteredPosts);
       } catch (error) {
         console.log(error);
       }
@@ -51,8 +54,8 @@ const MyProfile = () => {
   return (
     <Profile
       name='My'
-      desc='Personal Profile'
-      data={posts}
+      desc='Welcome to your profile page. Share prompts and inspire others with the power of your imagination'
+      data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
