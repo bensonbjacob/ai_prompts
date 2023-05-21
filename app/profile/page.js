f3fab08779edb5eb2c6ee2d4cdef2b72014d5108
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-05-21 15:20:47 -0500

    Add custom styled modal for delete confirmation

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,17 +1,17 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter, redirect } from 'next/navigation';
-
+import { useSession } from 'next-auth/react';
 import Profile from '@components/Profile';
-import Link from 'next/link';
 
 const MyProfile = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
 
   const [myPosts, setMyPosts] = useState([]);
+  const [postToDelete, setPostToDelete] = useState(null);
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
 
   if (!session) {
     redirect('/');
@@ -34,36 +34,62 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async (post) => {
-    const hasConfirmed = confirm(
-      'Are you sure you wish to delete this prompt?'
-    );
+  const handleDelete = (post) => {
+    setPostToDelete(post);
+    setIsConfirmModalOpen(true);
+  };
 
-    if (hasConfirmed) {
-      try {
-        await fetch(`/api/prompt/${post._id.toString()}`, {
-          method: 'DELETE',
-        });
+  const deletePost = async (post) => {
+    try {
+      await fetch(`/api/prompt/${post._id.toString()}`, {
+        method: 'DELETE',
+      });
 
-        const filteredPosts = myPosts.filter(
-          (item) => item._id !== post._id
-        );
-
-        setMyPosts(filteredPosts);
-      } catch (error) {
-        console.log(error);
-      }
+      const filteredPosts = myPosts.filter(
+        (item) => item._id !== post._id
+      );
+      setMyPosts(filteredPosts);
+    } catch (error) {
+      console.log(error);
     }
+
+    setIsConfirmModalOpen(false);
   };
 
   return (
-    <Profile
-      name='My'
-      desc='Welcome to your profile page. Share prompts and inspire others with the power of your imagination'
-      data={myPosts}
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
-    />
+    <div>
+      <Profile
+        name='My'
+        desc='Welcome to your profile page. Share prompts and inspire others with the power of your imagination'
+        data={myPosts}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
+
+      {isConfirmModalOpen && (
+        <div className='fixed inset-0 flex items-center justify-center z-50'>
+          <div className='bg-white p-4 rounded shadow-lg'>
+            <h3 className='text-lg font-semibold mb-2'>
+              Are you sure you wish to delete this prompt?
+            </h3>
+            <div className='flex justify-end'>
+              <button
+                className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded mr-2'
+                onClick={() => deletePost(postToDelete)}
+              >
+                Delete
+              </button>
+              <button
+                className='bg-gray-300 hover:bg-gray-400 text-gray-700 px-4 py-2 rounded'
+                onClick={() => setIsConfirmModalOpen(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
